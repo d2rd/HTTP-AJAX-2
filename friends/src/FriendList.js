5141f0b32d29435bd3c1c9a6bf87c0ee9d47a712
Glenn-David Daniel <d2rd@outlook.com>
2018-02-17 19:32:10 -0800

    WIP AJAXfull added FreindList.js

--- friends/src/FriendList.js
+++ friends/src/FriendList.js
@@ -1,9 +1,17 @@
 import React, { Component } from 'react';
 
 class FriendList extends React.Component {
+  state={
+    friends:[
+
+    ]
+  }
   render() {
     return <h1>My Friends</h1>
 
   }
 }
+
 export default FriendList;
+
+//http://localhost:5000/friends
