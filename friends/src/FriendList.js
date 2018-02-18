aa3c7567253c2682937ba2fbab0670c54cf979b6
Glenn-David Daniel <d2rd@outlook.com>
2018-02-17 20:11:40 -0800

    Displaying data from  FriendList.js

--- friends/src/FriendList.js
+++ friends/src/FriendList.js
@@ -1,14 +1,34 @@
-import React, { Component } from 'react';
+import React from 'react';
+import axios from 'axios';  // axios library is in server node_modules folder
 
 class FriendList extends React.Component {
-  state={
-    friends:[
 
-    ]
+state={
+    friends:[]
   }
   render() {
-    return <h1>My Friends</h1>
+    return (
+      <ul>
+        {this.state.friends.map((friend)=> {
+          return(
+          <li key={friend.id}>
+            {friend.name}
+          </li>
+        );
+        })}
+      </ul>
+      );
+  }
 
+// Coding convention: Place your helper methods (lifecycle hooks) after render().  These are things that the render() needs but do not have to execute before render().  This is a stylistic choice.
+
+//Helpers
+  componentDidMount() {
+    axios.get('http://localhost:5000/friends').then((response)=>{
+      this.setState({friends: response.data})
+    } ).catch(()=>{
+      console.error('error getting data') //use 'console.error' to show red icon and color in return
+    }) //always include .then and .catch so that you don't forget to included them.
   }
 }
 
