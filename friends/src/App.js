c8e97f5b9a0954ef84e2fdc585dafaa1ad86ee47
Glenn-David Daniel <d2rd@outlook.com>
2018-02-18 17:25:23 -0800

    WIP adding state and controls to FriendForm

--- friends/src/App.js
+++ friends/src/App.js
@@ -2,7 +2,7 @@ import React, { Component } from 'react';
 
 import './App.css';
 import FriendList from './FriendList';
-
+import FriendForm from './FriendForm';
 class App extends Component {
   render() {
     return (
